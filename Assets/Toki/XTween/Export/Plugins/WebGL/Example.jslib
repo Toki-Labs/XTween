c17e2bb30467ab14d0602aefdf5bd7f8773c84f4
R0biN-Desktop <dongmyung@toki-labs.com>
2018-12-21 01:09:32 +0900

    Refactoring path

---
+++ Assets/Toki/XTween/Export/Plugins/WebGL/Example.jslib
@@ -0,0 +1,31 @@
+﻿mergeInto(LibraryManager.library, {
+
+  Hello: function () {
+    window.alert("Hello, world!");
+  },
+
+  HelloString: function (str) {
+    window.alert(Pointer_stringify(str));
+  },
+
+  PrintFloatArray: function (array, size) {
+    for(var i = 0; i < size; i++)
+    console.log(HEAPF32[(array >> 2) + size]);
+  },
+
+  AddNumbers: function (x, y) {
+    return x + y;
+  },
+
+  StringReturnValueFunction: function () {
+    var returnStr = "bla";
+    var buffer = _malloc(lengthBytesUTF8(returnStr) + 1);
+    writeStringToMemory(returnStr, buffer);
+    return buffer;
+  },
+
+  /*BindWebGLTexture: function (texture) {
+    GLctx.bindTexture(GLctx.TEXTURE_2D, GL.textures[texture]);
+  },*/
+
+});
---
+++ Assets/Toki/XTween/Export/Scripts/WebGLCommutor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class WebGLCommutor : MonoBehaviour 
+{
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
+
+    [DllImport("__Internal")]
+    private static extern void PrintFloatArray(float[] array, int size);
+
+    [DllImport("__Internal")]
+    private static extern int AddNumbers(int x, int y);
+
+	[DllImport("__Internal")]
+    private static extern string GetString();
+
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    /*[DllImport("__Internal")]
+    private static extern void BindWebGLTexture(int texture); */
+
+	public Text text;
+
+    void Start() 
+	{
+		/*
+        Hello();
+        
+        HelloString("This is a string.");
+        
+        float[] myArray = new float[10];
+        PrintFloatArray(myArray, myArray.Length);
+        
+        int result = AddNumbers(5, 7);
+        Debug.Log(result);
+
+		this.text.text = StringReturnValueFunction();
+        
+        Debug.Log(StringReturnValueFunction());
+        
+        var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
+        BindWebGLTexture(texture.GetNativeTextureID()); */
+    }
+}
