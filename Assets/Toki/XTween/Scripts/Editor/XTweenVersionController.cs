e81cde2762424861566782f20ec6abc040db47ea
R0biN-Desktop <dongmyung@toki-labs.com>
2019-01-04 17:35:38 +0900

    Update fixed

--- Assets/Toki/XTween/Scripts/Editor/XTweenVersionController.cs
+++ Assets/Toki/XTween/Scripts/Editor/XTweenVersionController.cs
@@ -146,7 +146,7 @@ namespace Toki.Tween
 				string content = XTweenEditorManager.ReadText(namePath);
 				AssetDatabase.ImportPackage(filePath, false);
 				XTweenEditorManager.WriteText(namePath, content);
-				XTweenEditorManager.Instance.Load();
+				XTweenEditorManager.Instance.Data.version = this.StoredLastVersion;
 				EditorPrefs.DeleteKey(STORE_CHECKED_DATE);
 				EditorPrefs.DeleteKey(STORE_LAST_VERSION);
 			}
